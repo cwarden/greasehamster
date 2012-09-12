c9bdb8f332776c529d1a8ccff822da6289a400ff
Christian G. Warden <cwarden@xerus.org>
2012-09-12 15:18:02 -0700

    Show task email addresses in LiquidPlanner
    Makes email address of selected task always visible on the Projects page
    in LiquidPlanner.

---
+++ lp-email.user.js
@@ -0,0 +1,46 @@
+// ==UserScript==
+// @name          quick-lp-email
+// @namespace     http://greatvines.com
+// @description   Show LiquidPlanner Task email address in header
+// @match       https://app.liquidplanner.com/*
+//
+// ==/UserScript==
+
+// from http://bit.ly/guiCKN
+function addJQuery(callback) {
+  var script = document.createElement("script");
+  script.setAttribute("src", "https://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js");
+  script.addEventListener('load', function() {
+	  var script = document.createElement("script");
+	  script.textContent = "(" + callback.toString() + ")();";
+	  document.body.appendChild(script);
+  }, false);
+  document.body.appendChild(script);
+}
+
+// All your GM code must be inside this function
+function main() {
+	var $gm = $.noConflict(true);
+
+	var updateEmail = function() {
+		try {
+			var address = LP.Treeitem.grid.getSelectedRowId() + "-" + LP.JSON.emailInbox;
+			$gm('div#filter_cues span.email').remove();
+			$gm('div#filter_cues').append('<span class="email"><a href="mailto:' + address + '">' + address + '</a></span>');
+		} catch (e) {
+			console.log(e);
+		}
+	};
+	updateEmail();
+	$gm(document).on('click', 'td.ti_name', function() { setTimeout(updateEmail, 100); });
+	$gm(document).on('keyup', function() { setTimeout(updateEmail, 100); });
+}
+
+if (self != top) {
+	return;
+}
+
+// load jQuery and execute the main function
+addJQuery(main);
+
+
